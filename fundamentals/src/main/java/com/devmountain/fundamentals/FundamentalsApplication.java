d64bc25eb19f94c01b948ce01b7b9703ae057dc6
Wyatt Enourato <wenourato@gmail.com>
2022-01-25 10:41:15 -0700

    Initial commit

---
+++ fundamentals/src/main/java/com/devmountain/fundamentals/FundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.devmountain.fundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FundamentalsApplication.class, args);
+	}
+
+}
---
+++ fundamentals/src/test/java/com/devmountain/fundamentals/FundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devmountain.fundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FundamentalsApplicationTests {
+
+	@Tes NumberFormat currency = NumberFormat.getCurrencyInstance();t
+	void contextLoads() {
+	}
+
+}
